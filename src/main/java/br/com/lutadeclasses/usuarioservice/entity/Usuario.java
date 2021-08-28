b28a5bfab236c05fc58979117f6bb208dbc44055
Andrews Reis <andrewsdosreis@gmail.com>
2021-08-28 00:22:11 -0300

    Ajusta classe de Usuario

--- src/main/java/br/com/lutadeclasses/usuarioservice/entity/Usuario.java
+++ src/main/java/br/com/lutadeclasses/usuarioservice/entity/Usuario.java
@@ -11,8 +11,6 @@ import javax.persistence.Table;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-import br.com.lutadeclasses.usuarioservice.model.RoleEnum;
-
 @Entity
 @Table(name = "usuario", schema = "luta-de-classe-db")
 public class Usuario {
